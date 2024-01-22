bf19f35bd10984d307e1be1fdf0735a26088e65f
Reynaldo Rodriguez <reynaldojesus23@gmail.com>
2024-01-22 11:35:04 -0300

    Fix label

--- components/MovieListFull.tsx
+++ components/MovieListFull.tsx
@@ -7,7 +7,7 @@ export default async function MovieListFull() {
   return (
     <div>
       <h1 className="text-2xl font-bold mb-4 p-4 md:p-12 text-center">
-        Upcoming Movies
+        Popular Movies
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-4">
         {moviesResponse.results.map((movie, index) => (
